225fc9c05ec7aaf99062366062e6c4d2b1ea4e73
Luis Estebes <esgo@Luiss-MacBook-Pro-2.local>
2022-02-02 23:38:49 -0600

    se añadió bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require('express');
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+const path = require('path');
+var app = express();
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
